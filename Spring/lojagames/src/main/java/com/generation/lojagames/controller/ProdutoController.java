41aaff22b0656035ca8b23db174678543e0ac0ce
Paulounk <paulo.job@hotmail.com>
2021-07-30 12:28:27 -0300

    Loja de Games - API Rest

---
+++ Spring/lojagames/src/main/java/com/generation/lojagames/controller/ProdutoController.java
@@ -0,0 +1,64 @@
+package com.generation.lojagames.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.generation.lojagames.model.Produto;
+import com.generation.lojagames.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping("/produto")
+@CrossOrigin("*")
+public class ProdutoController {
+
+	private ProdutoRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> getAll(@PathVariable long id){
+		
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());		
+	}
+	
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Produto>> getByNome(@PathVariable String nome){
+		
+		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));	
+	}
+	
+
+	@PostMapping
+	public ResponseEntity<Produto> post(@RequestBody Produto produto){
+		
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(repository.save(produto));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produto> put(@RequestBody Produto produto){
+		
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(repository.save(produto));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id){
+		repository.deleteById(id);
+	}
+	
+}
